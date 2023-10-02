367e44ccca48c6b396768392c42d2c1d259cbe07
to_binio <tobias.frischmann1@gmail.com>
2023-10-02 11:55:43 +0200

    basic entity renderer

--- src/main/java/tobinio/realarrowtip/RealArrowTip.java
+++ src/main/java/tobinio/realarrowtip/RealArrowTip.java
@@ -3,6 +3,8 @@ package tobinio.realarrowtip;
 import net.fabricmc.api.ModInitializer;
 
 public class RealArrowTip implements ModInitializer {
+    public static String modID = "real_arrow_tip";
+
     /**
      * Runs the mod initializer.
      */
--- src/main/java/tobinio/realarrowtip/mixin/ProjectileEntityRendererMixin.java
+++ src/main/java/tobinio/realarrowtip/mixin/ProjectileEntityRendererMixin.java
@@ -2,11 +2,16 @@ package tobinio.realarrowtip.mixin;
 
 import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.client.render.OverlayTexture;
+import net.minecraft.client.render.RenderLayer;
 import net.minecraft.client.render.VertexConsumer;
 import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.entity.ProjectileEntityRenderer;
 import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.entity.projectile.ArrowEntity;
 import net.minecraft.entity.projectile.PersistentProjectileEntity;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.math.ColorHelper;
+import net.minecraft.util.math.RotationAxis;
 import org.joml.Matrix3f;
 import org.joml.Matrix4f;
 import org.spongepowered.asm.mixin.Mixin;
@@ -15,48 +20,40 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import tobinio.realarrowtip.RealArrowTip;
 
 @Mixin (ProjectileEntityRenderer.class)
 public abstract class ProjectileEntityRendererMixin<T extends PersistentProjectileEntity> {
+    @Inject (at = @At (value = "INVOKE", target = "Lnet/minecraft/client/util/math/MatrixStack;pop()V", shift = At.Shift.BEFORE), method = "render(Lnet/minecraft/entity/projectile/PersistentProjectileEntity;FFLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;I)V")
+    private void render(T persistentProjectileEntity, float f, float g, MatrixStack matrixStack,
+            VertexConsumerProvider vertexConsumerProvider, int i, CallbackInfo ci, @Local Matrix4f matrix4f,
+            @Local Matrix3f matrix3f) {
 
-    @Shadow
-    public abstract void vertex(Matrix4f positionMatrix, Matrix3f normalMatrix, VertexConsumer vertexConsumer, int x,
-            int y, int z, float u, float v, int normalX, int normalZ, int normalY, int light);
+        if (persistentProjectileEntity instanceof ArrowEntity arrowEntity) {
 
-    @Redirect (at = @At (value = "INVOKE", target = "Lnet/minecraft/client/render/entity/ProjectileEntityRenderer;vertex(Lorg/joml/Matrix4f;Lorg/joml/Matrix3f;Lnet/minecraft/client/render/VertexConsumer;IIIFFIIII)V", ordinal = 8), method = "render(Lnet/minecraft/entity/projectile/PersistentProjectileEntity;FFLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;I)V")
-    private void render1(ProjectileEntityRenderer<T> instance, Matrix4f positionMatrix, Matrix3f normalMatrix,
-            VertexConsumer vertexConsumer, int x, int y, int z, float u, float v, int normalX, int normalZ, int normalY,
-            int light) {
+            int color = arrowEntity.getColor();
 
-        this.vertexTint(positionMatrix, normalMatrix, vertexConsumer, -8, -2, 0, 0.0F, 0.0F, 0, 1, 0, light);
-    }
+            if (color < 0)
+                return;
 
-    @Redirect (at = @At (value = "INVOKE", target = "Lnet/minecraft/client/render/entity/ProjectileEntityRenderer;vertex(Lorg/joml/Matrix4f;Lorg/joml/Matrix3f;Lnet/minecraft/client/render/VertexConsumer;IIIFFIIII)V", ordinal = 9), method = "render(Lnet/minecraft/entity/projectile/PersistentProjectileEntity;FFLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;I)V")
-    private void render2(ProjectileEntityRenderer<T> instance, Matrix4f positionMatrix, Matrix3f normalMatrix,
-            VertexConsumer vertexConsumer, int x, int y, int z, float u, float v, int normalX, int normalZ, int normalY,
-            int light) {
+            VertexConsumer headVertexConsumer = vertexConsumerProvider.getBuffer(RenderLayer.getEntityCutout(new Identifier(RealArrowTip.modID, "textures/entity/projectiles/tipped_arrow_head.png")));
 
-        this.vertexTint(positionMatrix, normalMatrix, vertexConsumer, 8, -2, 0, 0.5F, 0.0F, 0, 1, 0, light);
-    }
+            int red = ColorHelper.Argb.getRed(color);
+            int blue = ColorHelper.Argb.getBlue(color);
+            int green = ColorHelper.Argb.getGreen(color);
 
-    @Redirect (at = @At (value = "INVOKE", target = "Lnet/minecraft/client/render/entity/ProjectileEntityRenderer;vertex(Lorg/joml/Matrix4f;Lorg/joml/Matrix3f;Lnet/minecraft/client/render/VertexConsumer;IIIFFIIII)V", ordinal = 10), method = "render(Lnet/minecraft/entity/projectile/PersistentProjectileEntity;FFLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;I)V")
-    private void render3(ProjectileEntityRenderer<T> instance, Matrix4f positionMatrix, Matrix3f normalMatrix,
-            VertexConsumer vertexConsumer, int x, int y, int z, float u, float v, int normalX, int normalZ, int normalY,
-            int light) {
-
-        this.vertexTint(positionMatrix, normalMatrix, vertexConsumer, 8, 2, 0, 0.5F, 0.15625F, 0, 1, 0, light);
-    }
-
-    @Redirect (at = @At (value = "INVOKE", target = "Lnet/minecraft/client/render/entity/ProjectileEntityRenderer;vertex(Lorg/joml/Matrix4f;Lorg/joml/Matrix3f;Lnet/minecraft/client/render/VertexConsumer;IIIFFIIII)V", ordinal = 11), method = "render(Lnet/minecraft/entity/projectile/PersistentProjectileEntity;FFLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;I)V")
-    private void render4(ProjectileEntityRenderer<T> instance, Matrix4f positionMatrix, Matrix3f normalMatrix,
-            VertexConsumer vertexConsumer, int x, int y, int z, float u, float v, int normalX, int normalZ, int normalY,
-            int light) {
-
-        this.vertexTint(positionMatrix, normalMatrix, vertexConsumer, -8, 2, 0, 0.0F, 0.15625F, 0, 1, 0, light);
+            for (int u = 0; u < 4; ++u) {
+                matrixStack.multiply(RotationAxis.POSITIVE_X.rotationDegrees(90.0F));
+                vertexTint(matrix4f, matrix3f, headVertexConsumer, -8, -2, 0, 0.0F, 0.0F, 0, 1, 0, i, red, green, blue);
+                vertexTint(matrix4f, matrix3f, headVertexConsumer, 8, -2, 0, 0.5F, 0.0F, 0, 1, 0, i, red, green, blue);
+                vertexTint(matrix4f, matrix3f, headVertexConsumer, 8, 2, 0, 0.5F, 0.15625F, 0, 1, 0, i, red, green, blue);
+                vertexTint(matrix4f, matrix3f, headVertexConsumer, -8, 2, 0, 0.0F, 0.15625F, 0, 1, 0, i, red, green, blue);
+            }
+        }
     }
 
     private void vertexTint(Matrix4f positionMatrix, Matrix3f normalMatrix, VertexConsumer vertexConsumer, int x, int y,
-            int z, float u, float v, int normalX, int normalZ, int normalY, int light) {
-        vertexConsumer.vertex(positionMatrix, (float) x, (float) y, (float) z).color(255, 0, 255, 255).texture(u, v).overlay(OverlayTexture.DEFAULT_UV).light(light).normal(normalMatrix, (float) normalX, (float) normalY, (float) normalZ).next();
+            int z, float u, float v, int normalX, int normalZ, int normalY, int light, int red, int green, int blue) {
+        vertexConsumer.vertex(positionMatrix, (float) x, (float) y, (float) z).color(red, green, blue, 255).texture(u, v).overlay(OverlayTexture.DEFAULT_UV).light(light).normal(normalMatrix, (float) normalX, (float) normalY, (float) normalZ).next();
     }
 }
